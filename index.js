aa023ba0e6a66d048616d94502b7aff0db15a79c
Minakshi Yadav <min07ster@gmail.com>
2018-04-17 14:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
-message = name +' is ' +height+' inches tall '
+message = name +' is ' +height+' inches tall than ram '
 
 module.exports = {name, height, message}
 console.log(message);
